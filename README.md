e8784dafab677a2848a16da32f9376ae19e63aae
Daniel Alvarez <daniel@alvarez.tech>
2020-09-30 17:42:59 -0400

    Remove support fragments

--- README.md
+++ README.md
@@ -4,10 +4,20 @@
 
 ### MapKit
 
+```groovy
+implementation 'com.huawei.hms:maps:5.0.2.300'
+```
+
+* The minimum Android API level (minSdkVersion) required for Map Kit is 19.
+* In the Map SDK 5.0.0.300 or later for Android, you must set an API key before initializing the map. You can directly copy the API key in api_key in the agconnect-services.json file, or call MapsInitializer.setApiKey(String apiKey) to set the API key.
+* To replace the Google Map SDK for Android of the V2 version with the HUAWEI Map SDK, you need to replace the package name `com.google.android.gms.maps` with `com.huawei.hms.maps`. To replace the Google Map SDK for Android of the V3 version with the HUAWEI Map SDK, you need to replace the package name com.google.android.libraries.maps with com.huawei.hms.maps. (To replace the package name, you can go to Edit > Find > Replace in Path in Android Studio.)
+
+* [FAQs](https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides-V5/faq-0000001050166999-V5)
+
 ## GMS
 
 ### Google Maps
 
 ```groovy
-implementation 'COM.google.android.gms:play-services-maps:17.0.0'
+implementation 'com.google.android.gms:play-services-maps:17.0.0'
 ```
--- app/src/main/java/tech/alvarez/mapsapp/MapActivity.java
+++ app/src/main/java/tech/alvarez/mapsapp/MapActivity.java
@@ -1,13 +1,13 @@
 package tech.alvarez.mapsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.huawei.hms.maps.HuaweiMap;
@@ -22,30 +22,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        MapFragment googleMapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.googleMap);
-        mapFragment.getMapAsync(this);
+        googleMapFragment.getMapAsync(this);
 
-        com.huawei.hms.maps.SupportMapFragment map2Fragment = (com.huawei.hms.maps.SupportMapFragment) getSupportFragmentManager()
+        com.huawei.hms.maps.MapFragment huaweiMapFragment = (com.huawei.hms.maps.MapFragment) getFragmentManager()
                 .findFragmentById(R.id.huaweiMap);
-        map2Fragment.getMapAsync(this);
+        huaweiMapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
 
-        LatLng sydney = new LatLng(-34, 151);
-        this.googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng officePosition = new LatLng(-17.760294, -63.201106);
+
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(officePosition)
+                .title("Huawei Bolivia Office")
+                .snippet("Av. Las Ramblas #100, Edificio Tropical Tours, 3er piso");
+
+        this.googleMap.addMarker(markerOptions);
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(officePosition));
     }
 
     @Override
     public void onMapReady(HuaweiMap huaweiMap) {
         this.huaweiMap = huaweiMap;
 
-        com.huawei.hms.maps.model.LatLng sydney = new com.huawei.hms.maps.model.LatLng (-34, 151);
-        this.huaweiMap.addMarker(new com.huawei.hms.maps.model.MarkerOptions().position(sydney).title("Hello"));
-        this.huaweiMap.moveCamera(com.huawei.hms.maps.CameraUpdateFactory.newLatLng(sydney));
+        com.huawei.hms.maps.model.LatLng officePosition = new com.huawei.hms.maps.model.LatLng(-17.760294, -63.201106);
+
+        com.huawei.hms.maps.model.MarkerOptions markerOptions = new com.huawei.hms.maps.model.MarkerOptions()
+                .position(officePosition)
+                .title("Huawei Bolivia Office")
+                .snippet("Av. Las Ramblas #100, Edificio Tropical Tours, 3er piso");
+
+        this.huaweiMap.addMarker(markerOptions);
+        this.huaweiMap.moveCamera(com.huawei.hms.maps.CameraUpdateFactory.newLatLng(officePosition));
     }
 }
